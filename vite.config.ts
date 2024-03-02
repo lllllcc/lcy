71aa760abeaa8b783f0f050b324d0cdfba6d33ef
llc <1978341025@qq.com>
2024-03-02 19:50:58 +0800

    lcy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import { dirname, resolve } from 'path'
 import { fileURLToPath } from 'url'
 import { defineConfig, loadEnv } from 'vite'
 const __dirname = dirname(fileURLToPath(import.meta.url))
-
+// eslint-disable-next-line no-control-regex
+const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g
+const DRIVE_LETTER_REGEX = /^[a-z]:/i
 // https://vitejs.dev/config/
 export default defineConfig(
   (
@@ -32,7 +34,23 @@ export default defineConfig(
           }
         }
       },
-      base: '/lcy/'
+      base: '/lcy/',
+      build: {
+        rollupOptions: {
+          output: {
+            // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+            sanitizeFileName(name) {
+              const match = DRIVE_LETTER_REGEX.exec(name)
+              const driveLetter = match ? match[0] : ''
+              // substr 是被淘汰語法，因此要改 slice
+              return (
+                driveLetter +
+                name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, '')
+              )
+            }
+          }
+        }
+      }
     }
   }
 )
